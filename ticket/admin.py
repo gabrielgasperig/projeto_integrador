dce947d3f78c6c59027d907ed8366d22f2007248
Gabriel Gasperi <gaabrielgasperi@gmail.com>
2025-08-22 16:46:57 -0300

    alter priority options ticket owner

--- ticket/admin.py
+++ ticket/admin.py
@@ -9,6 +9,10 @@ class TicketAdmin(admin.ModelAdmin):
     ordering = '-id',
     search_fields = 'id', 'title', 'description', 'priority', 'user',
     list_display_links = 'id',
-    list_filter = 'created_date'
+    list_filter = 'created_date',
     list_per_page = 10
     list_max_show_all = 200
+
+@admin.register(models.Priority)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = 'name',
--- ticket/models.py
+++ ticket/models.py
@@ -1,14 +1,34 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
 # Create your models here.
+class Priority(models.Model):
+    class Meta:
+        verbose_name = 'Priority'
+        verbose_name_plural = 'Priorities'        
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 class Ticket(models.Model):
     title = models.CharField(max_length=50)
-    priority = models.CharField(max_length=50)
-    description = models.TextField()
+    priority = models.ForeignKey(
+        Priority, 
+        on_delete=models.SET_NULL, 
+        blank=True, null=True)
     user = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, blank=True)
-    ceated_date = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField()
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    owner = models.ForeignKey(
+        User, 
+        on_delete=models.SET_NULL, 
+        blank=True, null=True)
     
     def __str__(self):
         return f'{self.title} | {self.description} | {self.priority}'
