ef0393c38380c15c9f3fd2b38512b6cef91bc03d
Gabriel <gaabrielgasperi@gmail.com>
2025-08-25 23:59:30 -0300

    creating head and header partials and configuring the search bar

--- ticket/urls.py
+++ ticket/urls.py
@@ -5,6 +5,7 @@ app_name = 'ticket'
 
 urlpatterns = [
     path('<int:ticket_id>/', views.ticket, name='ticket'),
+    path('search/', views.search, name='search'),
     path('', views.index, name='index'),
     
 ]
--- ticket/views/ticket_views.py
+++ ticket/views/ticket_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from ticket.models import Ticket
 
 # Create your views here.
@@ -7,6 +8,7 @@ def index(request):
 
     context = {
         'tickets': tickets,
+        'site_title': 'All Tickets',
     }
 
     return render(
@@ -14,6 +16,30 @@ def index(request):
         'ticket/index.html',
         context
     )
+
+def search(request):
+    search_value = request.GET.get('q', '')
+
+    if search_value == '':
+        return redirect('ticket:index')
+
+    tickets = Ticket.objects.filter(
+        show=True
+    ).filter(
+        Q(title__icontains=search_value) | Q(description__icontains=search_value)
+    ).order_by('-id')
+
+    context = {
+        'tickets': tickets,
+        'site_title': f'Search for "{search_value}"',
+    }
+
+    return render(
+        request,
+        'ticket/index.html',
+        context
+    )
+
 
 
 def ticket(request, ticket_id):
