6c7709a7baf6e80234afd0e6b212fa3f2ca75cf6
Gabriel Gasperi <gaabrielgasperi@gmail.com>
2025-08-26 17:32:35 -0300

    creating pagination and adjusting requests

--- ticket/views/ticket_views.py
+++ ticket/views/ticket_views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from ticket.models import Ticket
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
     tickets = Ticket.objects.filter(show=True).order_by('-id')
 
+    paginator = Paginator(tickets, 10)  # Show 10 tickets per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'tickets': tickets,
+        'page_obj': page_obj,
         'site_title': 'All Tickets',
     }
 
@@ -26,12 +31,17 @@ def search(request):
     tickets = Ticket.objects.filter(
         show=True
     ).filter(
-        Q(title__icontains=search_value) | Q(description__icontains=search_value)
+        Q(title__icontains=search_value) | Q(description__icontains=search_value) | Q(user__icontains=search_value)
     ).order_by('-id')
 
+    paginator = Paginator(tickets, 10)  # Show 10 tickets per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'tickets': tickets,
-        'site_title': f'Search for "{search_value}"',
+        'page_obj': page_obj,
+        'site_title': 'Search - ',
+        'search_value': search_value,
     }
 
     return render(
