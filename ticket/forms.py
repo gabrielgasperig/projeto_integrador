69a22387b2d7fcca0c4674c0ede28e985c9bd440
Gabriel Gasperi <gaabrielgasperi@gmail.com>
2025-08-28 11:17:20 -0300

    configuring form fields and widgets

--- ticket/forms.py
+++ ticket/forms.py
@@ -3,6 +3,18 @@ from django.core.exceptions import ValidationError
 from . import models
 
 class TicketForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.fields['title'].widget.attrs.update({'placeholder': 'Enter ticket title'})
+        self.fields['description'].widget.attrs.update({'placeholder': 'Enter ticket description'})
+        self.fields['description'].help_text = 'Enter ticket description'
+        self.fields['user'].widget.attrs.update({'placeholder': 'Select user'})
+
     class Meta:
         model = models.Ticket
         fields = 'title', 'description', 'priority','user', 
+
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 4}),
+        }
