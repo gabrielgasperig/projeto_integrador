657440bd4324739bcabf69cfad7566c908808b20
Gabriel <gaabrielgasperi@gmail.com>
2025-09-06 16:53:06 -0300

    validating first_name, last_name and user email fields

--- ticket/forms.py
+++ ticket/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
 from . import models
@@ -40,4 +41,20 @@ class TicketForm(forms.ModelForm):
         return title
 
 class RegisterForm(UserCreationForm):
-    ...
+    first_name = forms.CharField(required=True, min_length=3)
+    last_name = forms.CharField(required=True, min_length=3)
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = 'first_name', 'last_name', 'email', 'username', 'password1', 'password2',
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error('email', ValidationError(
+                'Email já cadastrado',
+                code='invalid'
+)) 
+
+        return email
