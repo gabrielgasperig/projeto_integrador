8751117d2d83c3c2eb375c84bf1eec7cf1622d2b
Gabriel <gaabrielgasperi@gmail.com>
2025-09-06 17:10:22 -0300

    using django.contrib.messages to send messages

--- ticket/views/user_forms.py
+++ ticket/views/user_forms.py
@@ -1,16 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
 
 from ticket.forms import RegisterForm
 
 def register(request):
     form = RegisterForm()
+    messages.info(request, 'Conta criada com sucesso!')
 
     if request.method == 'POST':
         form = RegisterForm(request.POST)
 
         if form.is_valid():
             form.save()
-            
+            messages.info(request, 'Conta criada com sucesso!')
+            return redirect('ticket:index')
+        
     return render(
         request, 
         'ticket/register.html',
